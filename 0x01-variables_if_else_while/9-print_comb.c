8710ec5159ace63353ed48daeca2101c22ae7383
POlagbe <polagbemide@gmail.com>
2023-03-18 01:22:33 -0700

    Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints all possible combinations of single-digit nos
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
