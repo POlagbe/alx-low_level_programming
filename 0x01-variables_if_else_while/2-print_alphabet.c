572b0bf2a0bfa7e97aa030e517951117cba88a0b
POlagbe <polagbemide@gmail.com>
2023-03-17 17:45:44 -0700

    I sometimes suffer from insomnia.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This programprints the alphabet in lowercase
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z'; i++)
+	{
+		putchar(i);
+	putchar('\n');
+	}
+	return (0);
+}
