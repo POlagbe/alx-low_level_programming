774bf187a9e00dd639f0e1704a1294b2e0514677
POlagbe <polagbemide@gmail.com>
2023-03-17 23:51:36 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This is a program that writes numbers of base 10
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
