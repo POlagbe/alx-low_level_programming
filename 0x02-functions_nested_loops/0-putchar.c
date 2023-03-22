678cb25f10ca1aaf8747e1d83e5822a60135c5e4
POlagbe <polagbemide@gmail.com>
2023-03-21 19:35:50 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+ * main - This is a program that prints _putchar
+ * _putchar- prints _putchar
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	_putchar(' ');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_puthcar('\n');
+	return (0);
+}
